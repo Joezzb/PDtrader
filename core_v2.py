1f517a0580890a6b2cb9239153de255cc68938b7
Joezzb <joezzb@163.com>
2022-11-25 18:06:50 +0800

    v2
    回测主体模块，多策略回测模块，风控模块,绘图模块。#佣金滑点还没写

---
+++ core_v2.py
@@ -0,0 +1,392 @@
+import numpy as np
+import pandas as pd
+import warnings
+import dataloader as dal
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mpl.rcParams.update(mpl.rcParamsDefault)
+
+# https://zhuanlan.zhihu.com/p/480348215
+# 一些默认配置，使得图表更美观
+large = 22; med = 16; small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+# style
+plt.style.use('seaborn-whitegrid')
+#sns.set_style("white")
+# 设置matplotlib正常显示中文
+plt.rcParams['font.sans-serif']=['SimHei']   # 用黑体显示中文
+plt.rcParams['axes.unicode_minus']=False 
+
+warnings.filterwarnings('ignore')
+
+tdt = pd.to_datetime
+
+# Backtracking core
+class PDcore():
+    def __init__(self,slist,dlist,cash=100000):
+        self.slist = slist
+        self.dlist = dlist
+
+        self.today = self.dlist.min()
+        self.position = {}
+        self.strat_position = {}
+        self.profit = pd.DataFrame(0,index=self.dlist,columns=self.slist)
+        self.init_cash = cash
+        self.cash = cash
+        self.trade = {}
+        self.balance = pd.DataFrame(0,index=self.dlist,columns=self.slist)
+        self.balance['cash'] = 0
+
+        self.x = 'open'
+        
+        for s in self.slist:
+            self.trade[s]={}
+        
+    def _run(self,row,amount,verbose = True):
+        s = row.code
+        d = row.date
+        # 记录每日结束现金余量
+        if self.today<d:
+            self.balance.loc[self.today,'cash'] = self.cash*1.
+            self.today = d    
+
+        if amount > 0:
+            self.buy(row,amount,verbose=verbose)
+            
+        elif amount < 0:
+            self.sell(row,-amount,verbose=verbose)
+
+    def buy(self,row,amount,verbose = True):
+        s = row.code
+        d = row.date
+        price = getattr(row,self.x)
+        if price*amount<=self.cash:
+            if s in self.position:
+                # 现金变动
+                self.cash -= price*amount
+                # 记录交易
+                self.trade[s][d] = [price,amount]
+                # 调整仓位
+                value = (self.position[s][2]+price*amount)
+                self.position[s] = [
+                    value/(amount + self.position[s][1]),
+                    amount + self.position[s][1],
+                    value
+                ]
+                if verbose:
+                    print('Date %s | Code %s | Amount %s | Add' % (d,s,amount))
+            else:
+                self.cash -= price*amount
+                self.position[s] = [price, amount,price*amount]
+                self.trade[s][d] = [price, amount]
+                if verbose:
+                    print('Date %s | Code %s | Amount %s | New' % (d,s,amount))
+
+    
+    def sell(self,row,amount,verbose=True):
+        s = row.code
+        d = row.date
+        price = getattr(row,self.x)
+        # 是否有仓位
+        if s in self.position:
+            #是否有足够库存
+            if self.position[s][1]>amount:
+                #现金变动
+                self.cash += price*amount
+                # 记录交易
+                self.trade[s][d]= [price,-amount]
+                # 调整仓位
+                self.position[s] = [
+                    self.position[s][0],
+                    self.position[s][1]-amount,
+                    self.position[s][2]-self.position[s][0]*amount
+                ]
+                # 计算利润
+                self.profit.loc[d,s] = (price-self.position[s][0])*amount
+                if verbose:
+                    print('Date %s | Code %s | Amount %s | Sell' % (d,s,amount))
+            else:
+                # 清仓
+                self.clear_position(row)
+                if verbose:
+                    print('Date %s | Code %s | Amount %s | Clear' % (d,s,amount))
+                
+    
+    def clear_position(self, row):
+        s = row.code
+        d = row.date
+        price = getattr(row,self.x)
+        #现金变动
+        self.cash += price*self.position[s][1]
+        # 记录交易
+        self.trade[s][d]= [price,-self.position[s][1]]
+        # 计算利润
+        self.profit.loc[d,s] = (price-self.position[s][0])*self.position[s][1]
+        # 清仓
+        del self.position[s]
+
+    def end(self,d):
+        # cash
+        self.balance.loc[d,'cash'] = self.cash*1.
+        # profit
+        self.stock_profit = self.profit.sum()
+        self.date_profit = self.profit.sum(axis=1)
+        self.value = self.balance.sum(axis=1)
+        self.drt=(self.value-self.value.shift(1).fillna(self.init_cash))/self.value
+        _ = self.sharpe()
+
+    def plot_value(self):
+        self.value.plot()
+        self.balance.cash.plot()
+        plt.title('Value and cash')
+        plt.ylim(bottom = 0)
+        plt.show()
+
+    def plot_rt(self):
+        cumsum = self.drt.cumsum()
+        sup = self.drt[0]
+        drawback = []
+        for i in cumsum:
+            if i>=sup:
+                sup = i
+                drawback.append(0)
+            else:
+                drawback.append(sup-i)
+        
+        fig,ax=plt.subplots(3,1,sharex=True)
+        ax[0].plot(cumsum)
+        ax[0].set_ylabel('Cum return')
+        ax[1].plot(self.balance.index,-np.array(drawback))
+        ax[1].set_ylabel('Drawback')
+        ax[2].scatter(self.drt)
+        ax[2].set_ylabel('Daily return')
+        ax[2].set_xlabel('Date')
+        fig.subplots_adjust(hspace=0.3)
+        fig.suptitle(f'Snapshot\n Sharpe ratio:{self.sharpe_ratio}')
+        plt.show()
+
+    def date_balance(self,date:str):
+        temp = self.balance.loc[pd.to_datetime(date)]
+        return temp[temp!=0]
+
+    def sharpe(self,rf=0,period=250):
+        riskfree = 0
+        if rf!=0:
+            print('RF')
+            riskfree = np.pow(1+rf,1/period)
+        _drt = self.drt-riskfree
+        self.sharpe_ratio =  _drt.mean()/_drt.std(ddof=1)*np.sqrt(period)
+        return self.sharpe_ratio
+
+    def add_risk(self,risk):
+        risk.init(slist=self.slist,x=self.x)
+        self.risk = risk
+
+# Backtracking cerebro
+class PDcerebro(PDcore):
+    def __init__(self,data,cash):
+        self.data = data.dropna().reset_index().sort_values(by=['date','code'])
+        self.slist = self.data.code.unique()
+        self.dlist = self.data.date.unique()
+        super().__init__(self.slist,self.dlist,cash)
+        
+        self.strategy = {}
+        self.pseduo_position = {}
+
+    def run(self,verbose = True):
+        if self.strategy:
+            # 有策略才运行
+            for row in self.data.itertuples():
+                s = row.code
+                d = row.date
+                price = getattr(row,self.x)
+                if self.risk:
+                    # 风控模块
+                    amount = self.risk.run(row,self.position)
+                    if amount <0:
+                        print('risk control',d)         
+                        for name , strat in self.strategy.items():
+                            _ = strat.execute(row,self.strat_position[name])
+                            self.adjust_strat_position(row,self.strat_position[name],amount)
+                            self._run(row,amount,verbose=verbose)
+                        self.risk.execute(s,d,price,amount,self.profit.loc[d,s])
+                    else:
+                        # 如风控无下单，正常交易            
+                        for name , strat in self.strategy.items():
+                            amount = strat.execute(row,self.strat_position[name])
+                            self.adjust_strat_position(row,self.strat_position[name],amount)
+                            self._run(row,amount,verbose=verbose)                        
+                else:
+                    # 如风控无下单，正常交易            
+                    for name , strat in self.strategy.items():
+                        amount = strat.execute(row,self.strat_position[name])
+                        self.adjust_strat_position(row,self.strat_position[name],amount)
+                        self._run(row,amount,verbose=verbose)
+
+                # 记录仓位价值
+                if s in self.position:
+                    self.balance.loc[d,s] = row.open*self.position[s][1]
+                    
+
+             # 最后一天总结
+            self.end(d)
+            for _ , strat in self.strategy.items():
+                strat.end(d)
+                
+        else:
+            print('No strategy')
+
+    def add_strats(self,strats,name = None):
+        strats.init(self.slist,self.dlist,self.cash)
+
+        if name:
+            pass
+        elif self.strategy:
+            name = len(self.strategy)
+        else:
+            name = 0
+            
+        self.strategy[name] = strats
+    
+        self.strat_position[name] = {}
+
+    def adjust_strat_position(self,row,position,amount):
+        # control the position of strategies in the cerebro
+        s = row.code
+        d = row.date
+        if s in position:
+            if position[s] + amount <=0:
+                del position[s]
+            else:
+                position[s]+= amount
+        else:
+            if amount >0:
+                position[s] = amount
+
+    def date_data(self,code,date):
+        # 代码日期截取股票数据
+        return self.data[(self.data.date==pd.to_datetime(date))&(self.data.code == code)]
+
+# Backtracking strategy
+class PDstrategy(PDcore):
+    def __init__(self,verbose = True):
+        self.active = True
+        self.verbose =verbose
+
+    def init(self, slist, dlist, cash=100000):
+        super().__init__(slist, dlist, cash)
+        self.active = True
+
+    def cal(self,row):
+        pass
+    
+    def execute(self,row,position):
+        s = row.code
+        d = row.date
+        if self.active:
+            # Strategy run
+            _amount = self.cal(row,self.position)
+            self._run(row,_amount,self.verbose)
+            # Record balance at the end of the day
+            if s in self.position:
+                self.balance.loc[d,s] = row.close*self.position[s][1]
+                
+        # Cerebro amount
+        return self.cal(row,position)
+
+# Sample strategy
+
+class alpha6(PDstrategy):
+    def __init__(self,date_trade):
+        super().__init__()
+        # date-trade dict
+        self.date_trade = date_trade
+        
+    def cal(self,row,position):
+        s = row.code
+        d = row.date
+        if s in position:
+            if s in self.date_trade[d]:
+                return 0
+            else:
+                return -100
+        else:
+            if s in self.date_trade[d]:
+                return 100
+            else:
+                return 0
+
+class PDrisk():
+    def __init__(self):
+        self.record = {}
+        self.blacklist = {}
+
+        self.max = {}
+        self.day = {}
+        
+    def init(self,slist,x):
+        for s in slist:
+            self.record[s] = {}
+        self.x = x
+
+    def run(self,row, position):
+        # keep track of indicators
+        s = row.code
+        d = row.date
+        price = getattr(row,self.x)
+        
+        # 风控黑名单（持续n天）
+        if s in self.blacklist:
+            self.blacklist[s] -= 1
+            if self.blacklist[s]<0:
+                del self.blacklist[s]
+            else:
+                return -999
+
+        if s in position:
+            if s in self.day:
+                self.max[s] = max(self.max[s],price)
+                self.day[s] += 1
+            else:
+                self.max[s] = max(position[s][0],price)
+                self.day[s] = 1
+            amount = self.cal(row,position)
+            return amount
+        else:
+            if s in self.day:
+                del self.max[s]
+                del self.day[s]
+            return 0
+
+    def cal(self,row,position):
+        # Risk control strategy
+        return 0
+
+    def execute(self,s,d,price,amount,profit):
+        self.record[s][d] = [price,amount,profit]
+
+class Cut(PDrisk):
+    def __init__(self,percent=0.10,n=10):
+        super().__init__()
+        self.percent = percent
+        self.n = n
+    def cal(self,row,position):
+        s = row.code
+        d = row.date
+        price = getattr(row,self.x)
+        if (self.max[s]-price)/self.max[s] >self.percent:
+            self.blacklist[s] = self.n
+            return -position[s][1]
+        else:
+            return 0
---
+++ dataloader.py
@@ -0,0 +1,220 @@
+import akshare as ak
+import numpy as np
+import pandas as pd
+import datetime as dt
+import time
+import os 
+from tqdm import tqdm
+import alphalens
+
+class dataloader():
+    def __init__(self):
+
+        # 待选股列表
+        self.slist = None
+        self.slist_path ='slist.csv'
+
+        #  历史成分股（不全）
+        self.slist_hist = None
+
+        # 股票信息
+        self.sinfo = None
+        self.sinfo_path = 'sinfo.csv'
+        
+        # 不考虑的股票
+        self.filtered_slist = None
+
+        # 股票数据 
+        self.sdata = None
+        self.sdata_path = 'sdata.csv'
+
+        self.slist_used = None
+
+        # 已加载因子和产生的交易
+        self.factor = []
+        self.trade = {}
+
+    def get_slist(self,symbol='000300'):
+        # 最新中正成分股（包含沪深300）
+        if self.slist is not None:
+            self.slist_used = self.slist.code.values
+            return self.slist
+        elif os.path.exists(self.slist_path):
+            # local file exist
+            self.slist = pd.read_csv(self.slist_path,encoding='gbk',dtype={'code':str})
+            self.slist_used = self.slist.code.values
+        else:
+            # extract info
+            self.slist = ak.index_stock_cons_csindex(symbol=symbol).iloc[:,[0,4,5]]
+            self.slist.columns = ['date','code','name']
+            self.slist.date = pd.to_datetime(self.slist.date)
+            self.slist.to_csv(self.slist_path,index=False,encoding='gbk')
+            self.slist_used = self.slist.code.values
+    
+    def get_slist_hist(self,symbol):
+        # index_stock_hist is bad function, data is incomplete.
+        try:
+            if symbol[:3]=='000':
+                self.slist_hist = ak.index_stock_hist(symbol='sz'+symbol)
+            elif symbol[:3]=='399':
+                self.slist_hist = ak.index_stock_hist(symbol='sh'+symbol)
+            else:
+                return 'Code not accepted'
+        except:
+            return 'Code not found'
+
+    def get_slist_info(self):
+        # 获取股票数据信息
+        if self.sinfo is not None:
+            return self.sinfo
+        elif os.path.exists(self.sinfo_path):
+            self.sinfo = pd.read_csv(self.sinfo_path,encoding='gbk',dtype={'股票代码':str})
+            self.sinfo['上市时间'] = pd.to_datetime(self.sinfo['上市时间'],format='%Y%m%d')
+        else:
+            self.sinfo = pd.DataFrame(columns=['总市值', '流通市值', '行业', '上市时间', '股票代码', '股票简称', '总股本', '流通股'])
+
+            # count for sleep
+            count = 0
+            for i in self.slist.code.unique():
+                self.sinfo.loc[len(self.sinfo)] = ak.stock_individual_info_em(symbol=i).value.values
+                if count%20 ==0:
+                    time.sleep(0.7)
+            self.sinfo['上市时间'] = pd.to_datetime(self.sinfo['上市时间'],format='%Y%m%d')
+            self.sinfo.to_csv(self.sinfo_path,index=False,encoding='gbk')
+
+            return self.sinfo
+
+    def filter_slist(self,date):
+        # 过滤上市在date之后的股票
+        if self.sinfo is None:
+           _ = self.get_slist_info()
+
+        self.filtered_slist = self.sinfo[self.sinfo['上市时间']>date].股票代码.sort_values().values
+        self.slist_used = self.slist_used[~np.isin(self.slist_used,self.filtered_slist)]
+        return self.filtered_slist
+
+    def get_sdata(self,start="2021/01/01",end='2021/01/231',adjust='qfq'):
+        # 获取股票数据
+        start_date=pd.to_datetime(start).strftime('%Y%m%d')
+        end_date=pd.to_datetime(end).strftime('%Y%m%d')
+        if self.sdata:
+            return self.sdata
+        elif os.path.exists(self.sdata_path):
+            self.sdata = pd.read_csv(self.sdata_path,dtype={'code':str})
+            self.sdata.date = pd.to_datetime(self.sdata.date)
+            self.sdata.set_index(['date','code'],inplace=True)
+            return self.sdata
+        else:
+            self.sdata=pd.DataFrame(columns=['日期', '开盘', '收盘', '最高', '最低', '成交量', '成交额', '振幅', '涨跌幅', '涨跌额', '换手率','code'])
+
+            # count for sleep
+            count = 0            
+            for i in tqdm(self.slist_used):
+                temp=ak.stock_zh_a_hist(symbol=i, period="daily", start_date=start_date, end_date=end_date, adjust=adjust)
+                temp['code']=i
+                self.sdata=self.sdata.append(temp,ignore_index=True)
+                if count%20 ==0:
+                    time.sleep(0.7)
+
+            self.sdata.drop(['振幅', '涨跌幅', '涨跌额', '换手率'], axis=1 , inplace=True)
+            self.sdata.columns = ['date','open','close','high','low','volume','amount','code']
+            self.sdata.volume = self.sdata.volume*100
+            self.sdata.date = pd.to_datetime(self.sdata.date)
+            self.sdata.to_csv(self.sdata_path,index=False,encoding='gbk')
+            
+            self.sdata.set_index(['date','code'],inplace=True)
+            
+            return self.sdata
+
+    def get_sdata_tdx(self,file_path,start="2021/01/01",end='2021/01/31'):
+        # 通达信数据
+        if self.sdata is not None:
+            return self.sdata
+        else:
+            self.sdata=pd.DataFrame()
+            for i in tqdm(self.slist_used):
+                temp = pd.read_csv(file_path+i+'.csv',header=None)
+                temp['code'] = i
+                self.sdata = self.sdata.append(temp)
+            self.sdata.columns = ['date','open','high','low','close','volume','amount','code']
+            self.sdata.date = pd.to_datetime(self.sdata.date)
+            self.sdata = self.sdata[(self.sdata.date>pd.to_datetime(start))&(self.sdata.date<pd.to_datetime(end))].reset_index(drop=True)
+
+            self.sdata.set_index(['date','code'],inplace=True)
+
+            return self.sdata
+            
+
+    def clean_tdx(self,path):
+        # 删除数据最后一行
+        for i in os.listdir(path):
+            temp = pd.read_csv(path+i,encoding='gbk',header=None)
+            temp = temp.iloc[:-1,:]
+            temp.to_csv(path+i,index=False,header=None)
+
+
+    def add_factor(self,x,name):
+        if name in self.sdata.columns:
+            print('Name exist')
+        else:
+            self.sdata[name] = x
+            self.factor.append(name)
+
+    def first_n(self,x,n):
+        return x[x.groupby(by='date').rank()<(n+1)]
+
+    def first_n_todict(self,name,n):
+        if name in self.factor:        
+            self.trade[name] = self.first_n(self.sdata[name],n).reset_index(level=0).groupby('date').groups
+            #return self.trade[name]
+
+    def date_trade(self,name,date):
+        return self.trade[name][pd.to_datetime(date)]
+        
+
+def sma(x,n):
+    return x.groupby(by='code').rolling(n).mean()
+
+def ts_sum(x,n):
+    return x.groupby(by='code').apply(lambda x: x.rolling(n).sum())
+
+def rank(x):
+    return x.groupby(by='date').rank(pct=True)
+
+def stddev(x,n):
+    return x.groupby(by='code').apply(lambda x: x.rolling(n).std())
+
+def covariance(x,y,n):
+    return pd.concat([x,y],axis=1).reset_index(level=1).groupby(by='code').rolling(n).cov().unstack().iloc[:,-2].reorder_levels(['date','code'])
+
+def correlation(x,y,n):
+    return pd.concat([x,y],axis=1).reset_index(level=1).groupby(by='code').rolling(n).corr().unstack().iloc[:,-2].reorder_levels(['date','code'])
+
+def shift_ndate(x,n):
+    return x.groupby(by='code').shift(n)
+
+def ts_return(x,n):
+    return x.groupby(by='code').apply(lambda x: x-x.shift(n))
+#==============================================
+# Formula
+def alpha_6(data):
+    return shift_ndate(-1*correlation(data.open,data.volume,10),1)
+
+def momentum_nd(data,n):
+    return shift_ndate(ts_return(data.close,n),1)
+
+#==============================================
+def alphalens_fullsheet(x):
+    if 'factor' in x.columns:
+        # The required format for alphalens.
+        x.reset_index(inplace=True)
+        x.index=pd.to_datetime(x.date)
+        x.set_index([x.index,x.code],inplace=True)
+
+        alpha_pri = x.pivot(index='date',columns='code',values='open')
+        alpha_pri.index = pd.to_datetime(alpha_pri.index)
+
+        ret = alphalens.utils.get_clean_factor_and_forward_returns(factor=x.factor,prices=alpha_pri)
+        alphalens.tears.create_full_tear_sheet(ret)
+
+    return ret
